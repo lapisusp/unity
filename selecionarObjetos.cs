bbc452ea1f4b5727d8b8f2acfc4ad24ffdbc40d7
Renan Vinicius <renan.ufg@gmail.com>
2016-10-22 14:39:54 -0200

    Create selecionarObjetos.cs
    Script para selecionar e movimentar objetos com o mouse

---
+++ selecionarObjetos.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class selecionarObjetos : MonoBehaviour
+{
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        if (this.tag == "Selecionavel"){ // verifica se o objeto tem a tag "Selecionável"
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        if (this.tag == "Selecionavel"){
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+            transform.position = curPosition;
+        }
+    }
+}
